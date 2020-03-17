26a9d34a891a7e2789dc4420943a700fd69b39e1
Alex de Melo Silva <alexmsilva@gmail.com>
2020-03-17 10:41:12 -0300

    Stats a second course and adds a new home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:bytebank/screens/transferencia/lista.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BytebankApp());
@@ -7,13 +6,55 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ListaTransferencias(),
+      home: Dashboard(),
       theme: ThemeData(
         primaryColor: Colors.green[900],
         accentColor: Colors.blueAccent[700],
         buttonTheme: ButtonThemeData(
           buttonColor: Colors.blueAccent[700],
           textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image.asset("images/bytebank_logo.png"),
+            Container(
+              width: 150.0,
+              height: 100.0,
+              padding: EdgeInsets.all(8.0),
+              color: Theme.of(context).primaryColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 32.0,
+                  ),
+                  Text(
+                    "Contatos",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
